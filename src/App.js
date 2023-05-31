9bb4a35ec1953f9b10ca6d55c8d21e6ba22735eb
Justin Johnson <justinjohnson@baggelMac.local>
2023-05-31 15:12:06 -0700

    react files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div classname="addPassword">
+        <input type='text' placeholder="Password" />
+        <input type='text' placeholder="Title" />
+        <button>Add Password</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
